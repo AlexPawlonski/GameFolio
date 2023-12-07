97ca6dd451e27fc66eaa09f5c96806aa0ac9fac6
Alex <apawlonski8@gmail.com>
2023-12-07 13:18:04 +0100

    server config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  css: ['~/main.css'],
+  devtools: {
+    enabled: true,
+  },
+  devServer: {
+    port: 8000, // par défaut: 3000
+  },
+  css: ["~/main.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
-})
+});
