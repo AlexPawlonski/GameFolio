02bd046476891c8be2c8c74dbbf88eb0cd896699
Alex <apawlonski8@gmail.com>
2023-12-07 12:58:10 +0100

    first commit

---
+++ app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="h-screen w-full bg-indigo-950 flex flex-col justify-center"
+  >
+    <div class="flex flex-col gap-10 ml-[15%]">
+      <div class="relative">
+        <p class="text-white text-6xl font-bold z-20">
+          Alex Pawlonski GameFolio
+        </p>
+        <p class="absolute top-1 text-6xl font-bold z-10 text-blue-300 blur-xl">
+          Alex Pawlonski GameFolio
+        </p>
+      </div>
+      <div class="relative">
+        <p class="text-white text-6xl font-bold z-20 text-st">
+          Coming soon ...
+        </p>
+        <p class="absolute top-1 text-6xl font-bold z-0 text-blue-300 blur-xl">
+          Coming soon ...
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
